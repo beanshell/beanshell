cdd7fb57b37bf2e155eba9586fcd2d58c2ae5867
Patrick Niemeyer <patniemeyer>
2000-09-06 20:33:45 +0000

    *** empty log message ***

---
+++ BeanShell/tests/Interactive/reload/Bar.java
@@ -0,0 +1,6 @@
+
+public class Bar {
+	public void run() {
+		System.out.println("Bar v2");
+	}
+}
---
+++ BeanShell/tests/Interactive/reload/Cat.java
@@ -0,0 +1,7 @@
+
+public class Cat extends Animal {
+	public void run() {
+		System.out.println("cat v2");
+		super.run();
+	}
+}
