9e01dc8a64b254581df75548ff0c0b95bbf26fa1
Patrick Niemeyer <patniemeyer@gmail.com>
2000-09-06 22:18:09 +0000

    *** empty log message ***

--- BeanShell/src/jarall.sh
+++ BeanShell/src/jarall.sh
@@ -1,4 +1,12 @@
-rm bsh/util/AWT*class
-jar cvfm bsh.jar Manifest.console bsh/lib/* bsh/util/*.class bsh/util/lib/* bsh/commands/*.class bsh/commands/*.bsh bsh/*.class bsh/classpath/*.class
+if [ "$1" ]
+then
+	jarname=$1
+else
+	jarname=bsh.jar
+fi
+
+rm -f bsh/util/AWT*class
+
+jar cvfm $jarname Manifest.console bsh/lib/* bsh/util/*.class bsh/util/lib/* bsh/commands/*.class bsh/commands/*.bsh bsh/*.class bsh/classpath/*.class
 
 
