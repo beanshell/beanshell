b3a17363f1e4c60c5028234dffcd016f0c3d81cd
Patrick Niemeyer <patniemeyer>
2000-09-09 20:50:44 +0000

    license blurbs

--- BeanShell/src/bsh/classpath/DiscreteFilesClassLoader.java
+++ BeanShell/src/bsh/classpath/DiscreteFilesClassLoader.java
@@ -1,3 +1,17 @@
+/*****************************************************************************
+ *                                                                           *
+ *  This file is part of the BeanShell Java Scripting distribution.          *
+ *  Documentation and updates may be found at http://www.beanshell.org/      *
+ *                                                                           *
+ *  BeanShell is distributed under the terms of the LGPL:                    *
+ *  GNU Library Public License http://www.gnu.org/copyleft/lgpl.html         *
+ *                                                                           *
+ *  Patrick Niemeyer (pat@pat.net)                                           *
+ *  Author of Exploring Java, O'Reilly & Associates                          *
+ *  http://www.pat.net/~pat/                                                 *
+ *                                                                           *
+ *****************************************************************************/
+
 package bsh.classpath;
 
 import java.io.*;
