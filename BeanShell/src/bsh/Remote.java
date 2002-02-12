22636aec10afd87c4c9f824070d59c6771f87710
Patrick Niemeyer <patniemeyer>
2002-02-12 05:58:58 +0000

    *** empty log message ***

---
+++ BeanShell/src/bsh/Remote.java
@@ -0,0 +1,117 @@
+/*****************************************************************************
+ *                                                                           *
+ *  This file is part of the BeanShell Java Scripting distribution.          *
+ *  Documentation and updates may be found at http://www.beanshell.org/      *
+ *                                                                           *
+ *  Sun Public License Notice:                                               *
+ *                                                                           *
+ *  The contents of this file are subject to the Sun Public License Version  *
+ *  1.0 (the "License"); you may not use this file except in compliance with *
+ *  the License. A copy of the License is available at http://www.sun.com    * 
+ *                                                                           *
+ *  The Original Code is BeanShell. The Initial Developer of the Original    *
+ *  Code is Pat Niemeyer. Portions created by Pat Niemeyer are Copyright     *
+ *  (C) 2000.  All Rights Reserved.                                          *
+ *                                                                           *
+ *  GNU Public License Notice:                                               *
+ *                                                                           *
+ *  Alternatively, the contents of this file may be used under the terms of  *
+ *  the GNU Lesser General Public License (the "LGPL"), in which case the    *
+ *  provisions of LGPL are applicable instead of those above. If you wish to *
+ *  allow use of your version of this file only under the  terms of the LGPL *
+ *  and not to allow others to use your version of this file under the SPL,  *
+ *  indicate your decision by deleting the provisions above and replace      *
+ *  them with the notice and other provisions required by the LGPL.  If you  *
+ *  do not delete the provisions above, a recipient may use your version of  *
+ *  this file under either the SPL or the LGPL.                              *
+ *                                                                           *
+ *  Patrick Niemeyer (pat@pat.net)                                           *
+ *  Author of Learning Java, O'Reilly & Associates                           *
+ *  http://www.pat.net/~pat/                                                 *
+ *                                                                           *
+ *****************************************************************************/
+
+
+package bsh;
+
+import java.io.*;
+import java.net.*;
+
+/**
+	Remoe executor class.
+*/
+public class Remote {
+
+	public static void main( String args[] ) 
+		throws Exception
+	{
+		if ( args.length < 2 ) {
+			System.out.println(
+				"usage: Remote URL(http|bsh) file [ file ] ... ");
+			System.exit(1);
+		}
+		String url = args[0];
+		if ( url.startsWith( "http:" ) ) {
+			String text = getFile(args[1]);
+			doHttp( url, text );
+		} else if ( url.startsWith( "bsh:" ) ) {
+			//doBsh( url, args );
+		} else
+			System.out.println( "Unrecognized URL type."
+				+"Scheme must be http:// or bsh://");
+	}
+
+	//static void doBsh( String url, String text ) { }
+
+	static void doHttp( String postURL, String text ) 
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append( "bsh.client=Remote" );
+		sb.append( "&bsh.script=" );
+		sb.append( URLEncoder.encode( text ) );
+		String formData = sb.toString(  );
+
+		try {
+		  URL url = new URL( postURL );
+		  HttpURLConnection urlcon =
+			  (HttpURLConnection) url.openConnection(  );
+		  urlcon.setRequestMethod("POST");
+		  urlcon.setRequestProperty("Content-type",
+			  "application/x-www-form-urlencoded");
+		  urlcon.setDoOutput(true);
+		  urlcon.setDoInput(true);
+		  PrintWriter pout = new PrintWriter( new OutputStreamWriter(
+			  urlcon.getOutputStream(), "8859_1"), true );
+		  pout.print( formData );
+		  pout.flush();
+
+		  // read results...
+		  int rc = urlcon.getResponseCode();
+		  if ( rc != HttpURLConnection.HTTP_OK )
+			System.out.println("Error, HTTP response: "+rc );
+
+		  BufferedReader bin = new BufferedReader( 
+			new InputStreamReader( urlcon.getInputStream() ) );
+		  String line;
+		  while ( (line=bin.readLine()) != null )
+			System.out.println( line );
+
+		} catch (MalformedURLException e) {
+		  System.out.println(e);     // bad postURL
+		} catch (IOException e2) {
+		  System.out.println(e2);    // I/O error
+		}
+	}
+
+	static String getFile( String name ) 
+		throws FileNotFoundException, IOException 
+	{
+		StringBuffer sb = new StringBuffer();
+		BufferedReader bin = new BufferedReader( new FileReader( name ) );
+		String line;
+		while ( (line=bin.readLine()) != null )
+			sb.append( line ).append( "\n" );
+		return sb.toString();
+	}
+
+}
