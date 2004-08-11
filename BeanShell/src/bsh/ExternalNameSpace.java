59976e8f8215f701e567a629fd0abba51a8d7e93
Patrick Niemeyer <patniemeyer>
2004-08-11 16:11:00 +0000

    external name space was not really doing its job... fixed now.

--- BeanShell/src/bsh/ExternalNameSpace.java
+++ BeanShell/src/bsh/ExternalNameSpace.java
@@ -101,6 +101,14 @@ public class ExternalNameSpace extends NameSpace
 
 	/**
 	*/
+	/*
+		Notes: This implmenetation of getVariableImpl handles the following
+		cases:
+		1) var in map not in local scope - var was added through map
+		2) var in map and in local scope - var was added through namespace
+		3) var not in map but in local scope - var was removed via map
+		4) var not in map and not in local scope - non-existent var
+	*/
     protected Variable getVariableImpl( String name, boolean recurse ) 
 		throws UtilEvalError
 	{
@@ -108,10 +116,29 @@ public class ExternalNameSpace extends NameSpace
 		Object value = externalMap.get( name );
 
 		Variable var;
-		if ( value == null ) // check parent namespace
+		if ( value == null ) 
+		{
+			// The var is not in external map and it should therefore not be
+			// found in local scope (it may have been removed via the map).  
+			// Clear it prophalactically.
+			super.unsetVariable( name ); 
+
+			// Search parent for var if applicable.
 			var = super.getVariableImpl( name, recurse );
-		else
-			var = new Variable( name, (Class)null, value, (Modifiers)null );
+		} else
+		{
+			// Var in external map may be found in local scope with type and
+			// modifier info.
+			Variable localVar = super.getVariableImpl( name, false );
+
+			// If not in local scope then it was added via the external map,
+			// we'll wrap it and pass it along.  Else we'll use the local
+			// version.
+			if ( localVar == null ) 
+				var = new Variable( name, (Class)null, value, (Modifiers)null );
+			else
+				var = localVar;
+		}
 
 		return var;
     }
