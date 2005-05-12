03b3039ad2ccf4a48aaacf0a5c48fde65fd28715
Patrick Niemeyer <patniemeyer@gmail.com>
2005-05-12 17:19:42 +0000

    continued work on type conversion, etc.

--- BeanShell/src/bsh/Reflect.java
+++ BeanShell/src/bsh/Reflect.java
@@ -676,59 +676,57 @@ class Reflect
         Implement JLS 15.11.2
 		Return the index of the most specific arguments match or -1 if no
 		match is found.
-		This method exists to factor out this common functionality from the
-		search for methods and constructors (which unfortunately don't share
-		a common interface for signature info).
-	 	@return the index of the most specific candidate
+		This method is used by both methods and constructors (which
+	 	unfortunately don't share a common interface for signature info).
+
+	 @return the index of the most specific candidate
+
+	 */
+	/*
+	 Note: Two methods which are equally specific should not be allowed by
+	 the Java compiler.  In this case BeanShell currently chooses the first
+	 one it finds.  We could add a test for this case here (I believe) by
+	 adding another isSignatureAssignable() in the other direction between
+	 the target and "best" match.  If the assignment works both ways then
+	 neither is more specific and they are ambiguous.  I'll leave this test
+	 out for now because I'm not sure how much another test would impact
+	 performance.  Method selection is now cached at a high level, so a few
+	 friendly extraneous tests shouldn't be a problem.
 	*/
 	static int findMostSpecificSignature(
 		Class [] idealMatch, Class [][] candidates )
 	{
-		Class [] bestMatch = null;
-		int bestMatchIndex = -1;
+		for ( int round = Types.FIRST_ROUND_ASSIGNABLE;
+			  round <= Types.LAST_ROUND_ASSIGNABLE; round++ )
+		{
+			Class [] bestMatch = null;
+			int bestMatchIndex = -1;
 
-		for (int i=0; i < candidates.length; i++) {
-			Class[] targetMatch = candidates[i];
-
-            /*
-                If idealMatch fits targetMatch and this is the first match
-				or targetMatch is more specific than the best match, make it
-				the new best match.
-            */
-			if ( isSignatureAssignable(idealMatch, targetMatch ) &&
-				((bestMatch == null) ||
-					isSignatureAssignable( targetMatch, bestMatch )))
+			for (int i=0; i < candidates.length; i++)
 			{
-				bestMatch = targetMatch;
-				bestMatchIndex = i;
+				Class[] targetMatch = candidates[i];
+
+				// If idealMatch fits targetMatch and this is the first match
+				// or targetMatch is more specific than the best match, make it
+				// the new best match.
+				if ( Types.isSignatureAssignable(
+						idealMatch, targetMatch, round )
+					&& ( (bestMatch == null) ||
+						Types.isSignatureAssignable( targetMatch, bestMatch,
+							Types.JAVA_BASE_ASSIGNABLE )
+						)
+				)
+				{
+					bestMatch = targetMatch;
+					bestMatchIndex = i;
+				}
 			}
+
+			if ( bestMatch != null )
+				return bestMatchIndex;
 		}
 
-		if ( bestMatch != null )
-			return bestMatchIndex;
-		else
-			return -1;
-	}
-
-	/**
-	 Is the 'from' signature (argument types) assignable to the 'to'
-	 signature (candidate method types) using isJavaAssignable()?
-	 This method handles the special case of null values in 'to' types
-	 indicating a loose type and matching anything.
-	 */
-	/*
-	Should check for strict java here and use isJavaAssignable() instead
-	*/
-	static boolean isSignatureAssignable( Class[] from, Class[] to )
-	{
-		if ( from.length != to.length )
-			return false;
-
-		for(int i=0; i<from.length; i++)
-			if ( !Types.isBshAssignable( to[i], from[i] ) )
-				return false;
-
-		return true;
+		return -1;
 	}
 
 	private static String accessorName( String getorset, String propName ) {
