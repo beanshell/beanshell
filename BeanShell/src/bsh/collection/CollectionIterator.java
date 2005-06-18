b6a2fec6264e31097520a80a2a006816545d270f
Daniel Leuck <dleuck>
2005-06-18 04:36:11 +0000

    Added support for use of the enhanced "for" loop over instances of the Iterable interface when using jave 5+.  This is consistent with the new JLS.

--- BeanShell/src/bsh/collection/CollectionIterator.java
+++ BeanShell/src/bsh/collection/CollectionIterator.java
@@ -1,5 +1,12 @@
 package bsh.collection;
 
+import bsh.Capabilities;
+import bsh.CollectionManager;
+import bsh.UtilTargetError;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
 import java.util.Iterator;
 import java.util.Collection;
 import java.util.Enumeration;
@@ -36,18 +43,20 @@ public class CollectionIterator implements bsh.BshIterator
 	/**
 	 * Create an iterator over the given object
 	 *
-	 * @param iterateOverMe Object of type Iterator, Collection, or types
-	 * supported by CollectionManager.BasicBshIterator
+	 * @param iterateOverMe Object of type Iterator, Collection,
+	 *     Iterable, or types supported by
+	 *     CollectionManager.BasicBshIterator
 	 *
 	 * @return an Iterator
 	 *
 	 * @throws java.lang.IllegalArgumentException If the argument is not a
-	 * supported (i.e. iterable) type.
+	 *     supported (i.e. iterable) type.
 	 *
 	 * @throws java.lang.NullPointerException If the argument is null
 	 */
-	protected Iterator createIterator(Object iterateOverMe) 
-	{
+	protected Iterator createIterator(Object iterateOverMe)
+		throws java.lang.IllegalArgumentException {
+		
 		if (iterateOverMe==null)
 			throw new NullPointerException("Object arguments passed to " +
 				"the CollectionIterator constructor cannot be null.");
@@ -57,15 +66,38 @@ public class CollectionIterator implements bsh.BshIterator
 
 		if (iterateOverMe instanceof Collection) 
 			return ((Collection)iterateOverMe).iterator();
-
+		
+		try {
+			Class c = Class.forName("java.lang.Iterable");
+			if(c.isInstance(iterateOverMe)) {
+				
+				try {
+					Method m = c.getMethod("iterator", new Class[0]);
+					return (Iterator)m.invoke(iterateOverMe, new Object[0]);
+				} catch(Throwable t) {
+					throw new RuntimeException("Unexpected problem calling " +
+						"\"iterator()\" on instance of java.lang.Iterable.", t);
+				}
+			}
+		} catch(ClassNotFoundException cnfe) {
+			// we are pre-Java 5 and should continue without an exception
+		} 
+		
 		/*
 		Should we be able to iterate over maps?
 		if (iterateOverMe instanceof Map)
 			return ((Map)iterateOverMe).entrySet().iterator();
 		*/
 
-		throw new IllegalArgumentException(
-			"Cannot enumerate object of type "+iterateOverMe.getClass());
+		final CollectionManager.BasicBshIterator bbi = new
+			CollectionManager.BasicBshIterator(iterateOverMe);
+		return new Iterator() {
+			public boolean hasNext() { return bbi.hasNext(); } 
+			public Object next() { return bbi.next(); }
+			public void remove() {
+				throw new UnsupportedOperationException( "remove() is not supported");
+			}
+		};
 	}
 
 	/**
