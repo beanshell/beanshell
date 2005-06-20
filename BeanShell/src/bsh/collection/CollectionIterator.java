6af0671ff78f115fc797f413b212bc90630dd1f0
Patrick Niemeyer <patniemeyer>
2005-06-20 00:15:47 +0000

    added Iterable test case.

--- BeanShell/src/bsh/collection/CollectionIterator.java
+++ BeanShell/src/bsh/collection/CollectionIterator.java
@@ -10,7 +10,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Iterator;
 import java.util.Collection;
 import java.util.Enumeration;
-//import java.util.Map;
 
 import java.lang.reflect.Array;
 
@@ -36,8 +35,8 @@ public class CollectionIterator implements bsh.BshIterator
 	 *
 	 * @throws java.lang.NullPointerException If the argument is null
 	 */	
-	public CollectionIterator(Object iterateOverMe) {
-		iterator = createIterator(iterateOverMe);	
+	public CollectionIterator( Object iterateOverMe ) {
+		iterator = createIterator( iterateOverMe );	
 	}
 
 	/**
@@ -54,50 +53,64 @@ public class CollectionIterator implements bsh.BshIterator
 	 *
 	 * @throws java.lang.NullPointerException If the argument is null
 	 */
-	protected Iterator createIterator(Object iterateOverMe)
-		throws java.lang.IllegalArgumentException {
+	protected Iterator createIterator( Object iterateOverMe )
+		throws java.lang.IllegalArgumentException 
+	{
 		
-		if (iterateOverMe==null)
+		if ( iterateOverMe == null )
 			throw new NullPointerException("Object arguments passed to " +
 				"the CollectionIterator constructor cannot be null.");
 
-		if (iterateOverMe instanceof Iterator)
+		if ( iterateOverMe instanceof Iterator )
 			return (Iterator)iterateOverMe;
 
-		if (iterateOverMe instanceof Collection) 
+		if ( iterateOverMe instanceof Collection ) 
 			return ((Collection)iterateOverMe).iterator();
 		
-		try {
-			Class c = Class.forName("java.lang.Iterable");
-			if(c.isInstance(iterateOverMe)) {
-				
-				try {
-					Method m = c.getMethod("iterator", new Class[0]);
-					return (Iterator)m.invoke(iterateOverMe, new Object[0]);
-				} catch(Throwable t) {
-					throw new RuntimeException("Unexpected problem calling " +
-						"\"iterator()\" on instance of java.lang.Iterable.", t);
-				}
-			}
-		} catch(ClassNotFoundException cnfe) {
-			// we are pre-Java 5 and should continue without an exception
-		} 
+		Iterator it = getIteratorForIterable( iterateOverMe );
+		if ( it != null )
+			return it;
 		
-		/*
-		Should we be able to iterate over maps?
-		if (iterateOverMe instanceof Map)
-			return ((Map)iterateOverMe).entrySet().iterator();
-		*/
-
 		final CollectionManager.BasicBshIterator bbi = new
 			CollectionManager.BasicBshIterator(iterateOverMe);
+
 		return new Iterator() {
 			public boolean hasNext() { return bbi.hasNext(); } 
 			public Object next() { return bbi.next(); }
 			public void remove() {
-				throw new UnsupportedOperationException( "remove() is not supported");
+				throw new UnsupportedOperationException( 
+					"remove() is not supported");
 			}
 		};
+	}
+
+	/**
+		Get an Iterator for a Java 5 Iterable object.
+		Rather than resorting to another loadable module for Java 5 we'll
+		use reflection here to invoke the Iterable iterator() method.
+
+		@return the Iterator or null if the object is not an Iterable.
+		@author Daniel Leuck
+	*/
+	Iterator getIteratorForIterable( Object iterateOverMe ) 
+	{
+		Iterator it = null;
+		try {
+			Class c = Class.forName("java.lang.Iterable");
+			if ( c.isInstance(iterateOverMe) ) 
+			{
+				try {
+					Method m = c.getMethod("iterator", new Class[0]);
+					it = (Iterator)m.invoke(iterateOverMe, new Object[0]);
+				} catch( Exception e ) {
+					throw new RuntimeException("Unexpected problem calling " +
+						"\"iterator()\" on instance of java.lang.Iterable.", e);
+				}
+			}
+		} catch( ClassNotFoundException cnfe ) {
+			// we are pre-Java 5 and should continue without an exception
+		} 
+		return it;
 	}
 
 	/**
