399975993106569cb8ca4f6efc35c77d7367e049
pfgiffunis@unal.edu.co <pfgiffunis@unal.edu.co@934af587-6f8e-29cc-0aa7-85b2284b99e2>
2012-12-13 19:44:13 +0000

    Update ASM to version 1.3.6
    Notable changes are:
    
    - ASM is now under a 3 clause BSD license.
    - Added equals and hashcode in Type.
    - Bug fixes.
    
    The only differences wrt the original distribution are:
    - prefixed the package with the bsh. to avoid collisions with original.
    - We don't carry CodeAdapter.java or ClassReader.java.
    - We don't include kASM jar.
    
    
    git-svn-id: https://svn.codespot.com/a/apache-extras.org/beanshell/trunk@9 934af587-6f8e-29cc-0aa7-85b2284b99e2

--- asm/src/bsh/org/objectweb/asm/ByteVector.java
+++ asm/src/bsh/org/objectweb/asm/ByteVector.java
@@ -1,21 +1,31 @@
 /***
  * ASM: a very small and fast Java bytecode manipulation framework
- * Copyright (C) 2000 INRIA, France Telecom
- * Copyright (C) 2002 France Telecom
+ * Copyright (c) 2000,2002,2003 INRIA, France Telecom
+ * All rights reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  *
  * Contact: Eric.Bruneton@rd.francetelecom.com
  *
@@ -274,7 +284,9 @@ final class ByteVector {
    */
 
   private void enlarge (final int size) {
-    byte[] newData = new byte[Math.max(2*data.length, length + size)];
+    int length1 = 2 * data.length;
+    int length2 = length + size;
+    byte[] newData = new byte[length1 > length2 ? length1 : length2];
     System.arraycopy(data, 0, newData, 0, length);
     data = newData;
   }
