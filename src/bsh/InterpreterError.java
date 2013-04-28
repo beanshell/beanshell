0f5185b132f010a037bb84ec7195f656be4fb53e
pejobo70@gmail.com <pejobo70@gmail.com@934af587-6f8e-29cc-0aa7-85b2284b99e2>
2013-04-28 21:15:47 +0000

    do not swallow exception cause - issue#4
    git-svn-id: https://svn.codespot.com/a/apache-extras.org/beanshell/trunk@18 934af587-6f8e-29cc-0aa7-85b2284b99e2

--- src/bsh/InterpreterError.java
+++ src/bsh/InterpreterError.java
@@ -25,17 +25,22 @@
  *****************************************************************************/
 
 
-
 package bsh;
 
 /**
-	An internal error in the interpreter has occurred.
-*/
-public class InterpreterError extends RuntimeException
-{
-	public InterpreterError(String s)
-	{
-		super(s);
-	}
+ * An internal error in the interpreter has occurred.
+ */
+public class InterpreterError extends RuntimeException {
+
+    public InterpreterError(String message) {
+        super(message);
+    }
+
+
+    public InterpreterError(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
 }
 
