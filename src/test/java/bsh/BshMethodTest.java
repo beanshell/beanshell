9a97a56a54449979dd31b0a68fd7bd858c05ac50
nickl- <github@jigsoft.co.za>
2017-10-02 10:19:28 +0200

    Merge beanshell2 fork commit: 477248a
    Merge pejobo/beanshell2@477248a fixes issue pejobo/beanshell2#28

--- src/test/java/bsh/BshMethodTest.java
+++ src/test/java/bsh/BshMethodTest.java
@@ -20,45 +20,55 @@
 
 package bsh;
 
-import org.junit.Assert;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 public class BshMethodTest {
 
-    /**
-     * Verifies that subclasses are not considered equal to superclass interfaces
-     * with a (potential) subset of the subclasses fields.
-     */
-    @SuppressWarnings("serial")
-    @Test
-    public void testEqualsObject_subclassEquality() {
-        // define a simple subclass of BshMethod:
-        class SubMethod extends BshMethod {
+   /**
+    * Verifies that subclasses are not considered equal to superclass interfaces
+    * with a (potential) subset of the subclasses fields.
+    */
+   @SuppressWarnings("serial")
+   @Test
+   public void testEqualsObject_subclassEquality() {
+      // define a simple subclass of BshMethod:
+      class SubMethod extends BshMethod {
+         public SubMethod(String name, Class returnType, String[] paramNames,
+               Class[] paramTypes, BSHBlock methodBody,
+               NameSpace declaringNameSpace, Modifiers modifiers) {
+            super(name, returnType, paramNames, paramTypes, methodBody,
+                  declaringNameSpace, modifiers);
+         }
+      }
+      final String name = "testMethod";
 
-            public SubMethod(String name, Class returnType, String[] paramNames, Class[] paramTypes, BSHBlock methodBody, NameSpace declaringNameSpace, Modifiers modifiers) {
-                super(name, returnType, paramNames, paramTypes, methodBody, declaringNameSpace, modifiers);
-            }
-        }
+      final BshMethod subInst =
+            new SubMethod(name, Integer.class, new String[] {}, new Class[] {},
+                  null, null, null);
+      final BshMethod supInst =
+            new BshMethod(name, Integer.class, new String[] {}, new Class[] {},
+                  null, null, null);
 
-        final String name = "testMethod";
-        final BshMethod subInst = new SubMethod(name, Integer.class, new String[]{}, new Class[]{}, null, null, null);
-        final BshMethod supInst = new BshMethod(name, Integer.class, new String[]{}, new Class[]{}, null, null, null);
+      Assert.assertFalse("Subclasses should not be equal to super classes",
+            supInst.equals(subInst));
+   }
 
-        Assert.assertFalse("Subclasses should not be equal to super classes", supInst.equals(subInst));
-    }
+   /**
+    * Very simple test to verify hashcode contract.
+    */
+   @Test
+   public void testHashCode_contract() {
+      final String name = "testMethod";
+      final BshMethod method1 = new BshMethod(name,
+            Integer.class, new String[]{}, new Class[]{}, null, null, null);
+      final BshMethod method2 = new BshMethod(name,
+            Integer.class, new String[]{}, new Class[]{}, null, null, null);
 
-
-    /**
-     * Very simple test to verify hashcode contract.
-     */
-    @Test
-    public void testHashCode_contract() {
-        final String name = "testMethod";
-        final BshMethod method1 = new BshMethod(name, Integer.class, new String[]{}, new Class[]{}, null, null, null);
-        final BshMethod method2 = new BshMethod(name, Integer.class, new String[]{}, new Class[]{}, null, null, null);
-
-        Assert.assertTrue("precondition check for test failed.", method2.equals(method1));
-        Assert.assertEquals("Equal classes should have equal hashcodes", method2.hashCode(), method1.hashCode());
-    }
+      Assert.assertTrue("precondition check for test failed.",
+            method2.equals(method1));
+      Assert.assertEquals("Equal classes should have equal hashcodes",
+            method2.hashCode(), method1.hashCode());
+   }
 }
