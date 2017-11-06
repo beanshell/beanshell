0a6bcf115b0adbc4e2858041144744cd4f104a8c
nickl- <github@jigsoft.co.za>
2017-11-07 01:26:46 +0200

    Merge beanshell2 fork commit: c51542b
    Merge pejobo/beanshell2@c51542b fix some compiler warnings

--- src/main/java/bsh/servlet/SimpleTemplate.java
+++ src/main/java/bsh/servlet/SimpleTemplate.java
@@ -69,7 +69,7 @@ public class SimpleTemplate
 {
     StringBuffer buff;
     static String NO_TEMPLATE = "NO_TEMPLATE";  // Flag for non-existent
-    static Map templateData = new HashMap();
+    static Map<String,String> templateData  = new HashMap<>();
     static boolean cacheTemplates = true;
 
     /**
@@ -84,7 +84,7 @@ public class SimpleTemplate
     */
     public static SimpleTemplate getTemplate( String file )
     {
-        String templateText = (String)templateData.get( file );
+        String templateText = templateData.get( file );
 
         if ( templateText == null || !cacheTemplates ) {
             try {
