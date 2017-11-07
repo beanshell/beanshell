eacb9dc5f0acb161025990cd1a07ef1e039059f9
nickl- <github@jigsoft.co.za>
2017-11-07 03:36:57 +0200

    Merge beanshell2 fork commit: 4df454b
    Merge pejobo/beanshell2@4df454b fix for pejobo/beanshell2#88 prevent obsolete calls to j.l.reflect.AccessibleObject.setAccessible(true)

--- src/main/java/bsh/Capabilities.java
+++ src/main/java/bsh/Capabilities.java
@@ -26,6 +26,7 @@
 
 package bsh;
 
+import java.lang.reflect.Field;
 import java.util.Hashtable;
 
 /**
@@ -73,6 +74,13 @@ public class Capabilities
             // test basic access
             try {
                 String.class.getDeclaredMethods();
+                try {
+                    final Field field = Capabilities.class.getField("classes");
+                    field.setAccessible(true);
+                    field.setAccessible(false);
+                } catch (NoSuchFieldException e) {
+                    // ignore
+                }
             } catch ( SecurityException e ) {
                 throw new Unavailable("Accessibility unavailable: "+e, e);
             }
