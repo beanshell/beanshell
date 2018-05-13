c5589a798d8b705e3f24df60c2ec75caa740b10d
nickl- <github@jigsoft.co.za>
2018-05-13 14:26:27 +0200

    Merge beanshell2 fork commit: 797ee8a
    Merge pejobo/beanshell2@797ee8a fix for pejobo/beanshell2#88
    test case for pejobo/beanshell2##97, pejobo/beanshell2#98, and pejobo/beanshell2##99

--- src/test/java/bsh/Issue_88_Test.java
+++ src/test/java/bsh/Issue_88_Test.java
@@ -3,16 +3,37 @@ package bsh;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static bsh.TestUtil.eval;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(FilteredTestRunner.class)
 public class Issue_88_Test {
 
     @Test
-    public void call_of_public_inherited_method_from_non_public_class() throws Exception {
+    public void call_of_public_inherited_method_from_non_public_class_without_accessibilty() throws Exception {
+        boolean current = Capabilities.haveAccessibility();
+        Capabilities.setAccessibility(false);
         final Interpreter interpreter = new Interpreter();
         interpreter.set("x", new Implementation());
-        assertEquals("public", interpreter.eval("x.method(\"foo\");"));
+        assertEquals("public String", interpreter.eval("x.method(\"foo\");"));
+        Capabilities.setAccessibility(current);
+    }
+
+    @Test
+    public void call_of_public_inherited_method_from_non_public_class() throws Exception {
+        boolean current = Capabilities.haveAccessibility();
+        Capabilities.setAccessibility(true);
+       final Interpreter interpreter = new Interpreter();
+        interpreter.set("x", new Implementation());
+        assertEquals("public String", interpreter.eval("x.method(\"foo\");"));
+        Capabilities.setAccessibility(current);
+    }
+
+    @Test
+    public void community_test_cases() throws Exception {
+        assertEquals(0, eval("Collections.unmodifiableList(new ArrayList()).size();"));
+        assertEquals(0, eval("new HashMap().entrySet().size();"));
+        assertEquals(Boolean.FALSE, eval("new HashMap().keySet().iterator().hasNext();"));
     }
 
 
@@ -20,16 +41,29 @@ public class Issue_88_Test {
         Object method(String param);
     }
 
-    abstract class AbstractImplementation implements Public {
+
+    public interface PublicWithoutMethod extends Public {
+    }
+
+
+    private abstract class AbstractImplementation implements PublicWithoutMethod {
         public Object method(final String param) {
-            return "public";
+            return "public String";
+        }
+
+        private Object method(final Object param) {
+            return "private Object";
         }
     }
+
 
     public class Implementation extends AbstractImplementation {
-        private Object method(final Object param) {
-            return "private";
+        public Object method(final CharSequence param) {
+            return "public CharSequence";
+        }
+
+        public Object method(final Object param) {
+            return "public Object";
         }
     }
-
 }
