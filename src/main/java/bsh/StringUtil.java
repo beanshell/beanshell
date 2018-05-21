e310f7c35e624ecb01655fac2789091dda8b7a15
nickl- <github@jigsoft.co.za>
2018-05-21 22:03:38 +0200

    Enhanced for, collection manager and reflection.
     * Rewrote the enhanced for test case with elaborate coverage. Shows loop block scope and state, and details of type iterations.
     * Improved Collection manager:
     ** Added iterate over number range from 0 up to number or 0 down to number.
     ** Added unicode character code point range with nearest block set start. iterate over a - z, A - Z, 0 - 9, \ u16A0 - \ u16F8 etc...
     ** Reverted back to iterate over a string representation of type member reflection details, instead of Map.Entry items. Includes class declaration definition and produces the true bsh information for generated classes. Allows reflection iteration via class type, object instance or class identifier.
     ** Ensure everything and anything is iterable.
     * Improved modifiers class using ordered list instead of hashtable and improved toString for reuse.
     * Improved StringUtil method string output.

--- src/main/java/bsh/StringUtil.java
+++ src/main/java/bsh/StringUtil.java
@@ -25,37 +25,135 @@
  *****************************************************************************/
 package bsh;
 
-public class StringUtil
-{
-    public static String maxCommonPrefix( String one, String two ) {
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class StringUtil {
+
+    /** Count region matches.
+     * @param one string to compare
+     * @param two string to compare with
+     * @return return max common string */
+    public static String maxCommonPrefix(String one, String two) {
         int i=0;
-        while( one.regionMatches( 0, two, 0, i ) )
+        while( one.regionMatches(0, two, 0, i ))
             i++;
         return one.substring(0, i-1);
     }
 
-    public static String methodString(String name, Class[] types)
-    {
+    /** Produce a simple string representation of a method name with args.
+     * @param name the method name
+     * @param types the parameter type args
+     * @return string representation of a method */
+    public static String methodString(String name, Class<?>[] types) {
         StringBuilder sb = new StringBuilder();
-        sb.append(name);
-        sb.append('(');
-        for( int i=0; i<types.length; i++ )
-        {
-            Class c = types[i];
-            if (i != 0) {
+        sb.append(name).append('(');
+        for( int i=0; i<types.length; i++ ) {
+            if (i != 0)
                 sb.append(", ");
-            }
-            sb.append((c == null) ? "null" : c.getName());
+            sb.append((types[i] == null) ? "Object" : types[i].getSimpleName());
         }
         sb.append(')');
         return sb.toString();
     }
 
-    /**
-        Hack - The real method is in Reflect.java which is not public.
-    */
-    public static String normalizeClassName( Class type )
-    {
+    /** Produce a complete string representation of a reflect method. Shows
+     * modifiers, return type, name and parameter types.
+     * @param method a java reflect method
+     * @return string representation of a method */
+    public static String methodString(Method method) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Modifier.toString(method.getModifiers()))
+            .append(" ").append(method.getReturnType().getSimpleName())
+            .append(" ")
+            .append(methodString(method.getName(), method.getParameterTypes()));
+        return sb.toString();
+    }
+
+    /** Produce a complete string representation of a bsh method. Shows
+     * modifiers, return type, name and parameter types.
+     * @param method a bsh method
+     * @return string representation of a method */
+    public static String methodString(BshMethod method) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(method.modifiers.toString().substring(11))
+            .append(" ").append(method.getReturnType().getSimpleName())
+            .append(" ")
+            .append(methodString(method.getName(), method.getParameterTypes()));
+        return sb.toString();
+    }
+
+    /** Produce a string representation of a bsh generated class declaration.
+     * Shows modifiers, name, extends and implements.
+     * @param type the class to reflect
+     * @return string representation of a class declaration */
+    private static String generatedClassString(Class<?> type) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Reflect.getClassModifiers(type).toString().substring(11))
+          .append(type.isInterface() ? " interface" : " class")
+          .append(" ").append(type.getSimpleName());
+        if (type.getSuperclass() != null)
+            sb.append(" extends ").append(type.getSuperclass().getSimpleName());
+        if (type.getInterfaces().length > 0)
+            sb.append(" implements");
+            for (Class<?> i : type.getInterfaces())
+                sb.append(" ").append(i.getSimpleName());
+        return sb.toString();
+    }
+
+    /** Produce a string representation of a class declaration.
+     * Verifies if type is a generated class else builds a java
+     * reflect class string definition.
+     * Shows modifiers, name, extends and implements.
+     * @param type the class to reflect
+     * @return string representation of a class declaration */
+    public static String classString(Class<?> type) {
+        if (Reflect.isGeneratedClass(type))
+            return generatedClassString(type);
+        StringBuilder sb = new StringBuilder();
+        sb.append(Modifier.toString(type.getModifiers()))
+            .append(type.isInterface() ? "": " class")
+            .append(" ").append(type.getSimpleName());
+        if (type.getSuperclass() != null)
+            sb.append(" extends ").append(type.getSuperclass().getSimpleName());
+        if (type.getInterfaces().length > 0) {
+            sb.append(" implements ");
+            for (Class<?> i : type.getInterfaces())
+                sb.append(i.getSimpleName()).append(" ");
+        }
+        return sb.toString();
+    }
+
+    /** Produce a string representation of a bsh variable declaration.
+     * Shows modifiers, type and name.
+     * @param var the variable to reflect
+     * @return string representation of variable declaration */
+    public static String variableString(Variable var) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(var.modifiers.toString().substring(11))
+            .append(" ").append(var.type.getSimpleName())
+            .append(" ").append(var.name);
+        return sb.toString();
+    }
+
+    /** Produce a string representation of a java field declaration.
+     * Shows modifiers, type and name.
+     * @param var the field to reflect
+     * @return string representation of field declaration */
+    public static String variableString(Field field) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Modifier.toString(field.getModifiers()))
+            .append(" ").append(field.getType().getSimpleName())
+            .append(" ").append(field.getName());
+        return sb.toString();
+    }
+
+    /** Expose package private class Reflect normalizeClassName method
+     * for commands. Delegates to the Reflect class.
+     * @param type the value to delegate.
+     * @return delegated response */
+    public static String normalizeClassName( Class<?> type ) {
         return Reflect.normalizeClassName( type );
     }
 }
