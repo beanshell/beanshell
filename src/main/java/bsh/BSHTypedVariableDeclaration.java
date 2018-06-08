853b55bd084b5f0760f7fd603f665fa073a1d0c9
nickl- <github@jigsoft.co.za>
2018-06-08 06:15:38 +0200

    Collect fields and retain variable virtual modifiers.
    * Once the class has been generated fields will be referenced from the class, doing it here retains the modifiers state.

--- src/main/java/bsh/BSHTypedVariableDeclaration.java
+++ src/main/java/bsh/BSHTypedVariableDeclaration.java
@@ -27,8 +27,8 @@
 
 package bsh;
 
-class BSHTypedVariableDeclaration extends SimpleNode
-{
+class BSHTypedVariableDeclaration extends SimpleNode {
+    private static final long serialVersionUID = 1L;
     public Modifiers modifiers = new Modifiers(Modifiers.FIELD);
     private BSHVariableDeclarator[] bvda;
 
@@ -38,7 +38,7 @@ class BSHTypedVariableDeclaration extends SimpleNode
         return ((BSHType)jjtGetChild(0));
     }
 
-    Class evalType( CallStack callstack, Interpreter interpreter )
+    Class<?> evalType( CallStack callstack, Interpreter interpreter )
         throws EvalError
     {
         BSHType typeNode = getTypeNode();
@@ -60,7 +60,7 @@ class BSHTypedVariableDeclaration extends SimpleNode
     }
 
     /**
-        evaluate the type and one or more variable declarators, e.g.:
+        evaluate the type and one or more variable declarations, e.g.:
             int a, b=5, c;
     */
     public Object eval( CallStack callstack, Interpreter interpreter)
@@ -70,7 +70,7 @@ class BSHTypedVariableDeclaration extends SimpleNode
             NameSpace namespace = callstack.top();
             BSHType typeNode = getTypeNode();
             typeNode.declaredDimensions(getDeclarators()[0].dimensions);
-            Class type = typeNode.getType( callstack, interpreter );
+            Class<?> type = typeNode.getType( callstack, interpreter );
 
             BSHVariableDeclarator [] bvda = getDeclarators();
             for (int i = 0; i < bvda.length; i++)
@@ -80,10 +80,27 @@ class BSHTypedVariableDeclaration extends SimpleNode
                 // Type node is passed down the chain for array initializers
                 // which need it under some circumstances
                 Object value = dec.eval( typeNode, callstack, interpreter);
-
                 try {
-                    namespace.setTypedVariable(
-                        dec.name, type, value, modifiers );
+                    LHS lhs = null;
+                    if ( namespace.isClass )
+                        if ( null != namespace.classInstance )
+                            lhs = new LHS(namespace.classInstance,
+                                Reflect.resolveJavaField(
+                                    namespace.classInstance.getClass(),
+                                dec.name, modifiers.hasModifier("static")));
+                        else if ( null != namespace.classStatic )
+                            lhs = new LHS(namespace.classStatic,
+                                Reflect.resolveJavaField(namespace.classStatic,
+                                dec.name, modifiers.hasModifier("static")));
+
+                    if ( null != lhs && null != lhs.field ) {
+                        Variable var = new Variable(dec.name, type, lhs);
+                        var.modifiers = modifiers;
+                        var.setValue(value, Variable.ASSIGNMENT);
+                        namespace.setVariableImpl(var);
+                    } else
+                        namespace.setTypedVariable(
+                                dec.name, type, value, modifiers );
                 } catch ( UtilEvalError e ) {
                     throw e.toEvalError( this, callstack );
                 }
