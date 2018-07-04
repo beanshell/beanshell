0b1543a1072faec46ccb0f02fbba3383d75538d3
nickl- <github@jigsoft.co.za>
2018-07-04 15:02:27 +0200

    Set caller info for assert fail.

--- src/main/java/bsh/BSHMethodInvocation.java
+++ src/main/java/bsh/BSHMethodInvocation.java
@@ -51,6 +51,10 @@ class BSHMethodInvocation extends SimpleNode
         NameSpace namespace = callstack.top();
         BSHAmbiguousName nameNode = getNameNode();
 
+      // get caller info for assert fail
+      if ("fail".equals(nameNode.text))
+          interpreter.getNameSpace().setNode(this);
+
         // Do not evaluate methods this() or super() in class instance space
         // (i.e. inside a constructor)
         if ( namespace.getParent() != null && namespace.getParent().isClass
