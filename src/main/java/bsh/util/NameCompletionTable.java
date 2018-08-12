3cf08ab03b998109fb3793fe251d7cb58df25e1d
Vaclav Haisman <vhaisman@gmail.com>
2018-08-12 08:29:07 +0200

    Generify NameCompletionTable.

--- src/main/java/bsh/util/NameCompletionTable.java
+++ src/main/java/bsh/util/NameCompletionTable.java
@@ -41,7 +41,7 @@ public class NameCompletionTable extends ArrayList
 {
     /** Unimplemented - need a collection here */
     NameCompletionTable table;
-    List sources;
+    List<NameSource> sources;
 
     /** Unimplemented - need a collection of sources here*/
 
@@ -72,7 +72,7 @@ public class NameCompletionTable extends ArrayList
             monitors it by registering a listener
         */
         if ( sources == null )
-            sources = new ArrayList();
+            sources = new ArrayList<>();
 
         sources.add( source );
     }
@@ -80,7 +80,7 @@ public class NameCompletionTable extends ArrayList
     /**
         Add any matching names to list (including any from other tables)
     */
-    protected void getMatchingNames( String part, List found )
+    protected void getMatchingNames( String part, List<String> found )
     {
         // check our table
         for( int i=0; i< size(); i++ ) {
@@ -99,7 +99,7 @@ public class NameCompletionTable extends ArrayList
         if ( sources != null )
             for( int i=0; i< sources.size(); i++ )
             {
-                NameSource src = (NameSource)sources.get(i);
+                NameSource src = sources.get(i);
                 String [] names = src.getAllNames();
                 for( int j=0; j< names.length; j++ )
                     if ( names[j].startsWith( part ) )
@@ -110,17 +110,17 @@ public class NameCompletionTable extends ArrayList
 
     public String [] completeName( String part )
     {
-        List found = new ArrayList();
+        List<String> found = new ArrayList<String>();
         getMatchingNames( part, found );
 
         if ( found.size() == 0 )
             return new String [0];
 
         // Find the max common prefix
-        String maxCommon = (String)found.get(0);
+        String maxCommon = found.get(0);
         for(int i=1; i<found.size() && maxCommon.length() > 0; i++) {
             maxCommon = StringUtil.maxCommonPrefix(
-                maxCommon, (String)found.get(i) );
+                maxCommon, found.get(i));
 
             // if maxCommon gets as small as part, stop trying
             if ( maxCommon.equals( part ) )
