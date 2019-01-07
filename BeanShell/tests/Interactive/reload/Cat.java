102ddbae99260ad664b38c76b734e2f6869ea007
nickl- <github@jigsoft.co.za>
2019-01-07 20:25:09 +0200

    Deleted artifacts inherited from merged repos

--- BeanShell/tests/Interactive/reload/Cat.java
+++
@@ -1,7 +0,0 @@
-
-public class Cat extends Animal {
-	public void run() {
-		System.out.println("cat v2");
-		super.run();
-	}
-}
--- BeanShell/tests/Interactive/reload/Foo.java
+++
@@ -1,7 +0,0 @@
-
-public class Foo {
-	public void run() {
-		System.out.println("Foo v3");
-		new Bar().run();
-	}
-}
