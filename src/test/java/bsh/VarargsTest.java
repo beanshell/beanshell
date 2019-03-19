3e4da04d47f899d060c8bffce8c92b693f0760be
nickl- <github@jigsoft.co.za>
2019-03-19 09:38:53 +0200

    Test second argument varargs.

--- src/test/java/bsh/VarargsTest.java
+++ src/test/java/bsh/VarargsTest.java
@@ -61,16 +61,26 @@ public class VarargsTest {
     }
 
 
+    @Test
+    public void calling_java_varargs_2nd_argument() throws Exception {
+        final Interpreter interpreter = new Interpreter();
+        interpreter.set("helper", new ClassWithVarargMethods());
+        @SuppressWarnings({"unchecked"})
+        final List<Object> list = (List<Object>) interpreter.eval("helper.list((List)new ArrayList(), 1, 2, 3)");
+        Assert.assertEquals(Arrays.<Object>asList(1,2,3), list);
+        interpreter.close();
+    }
+
     public static class ClassWithVarargMethods {
-
-        public List<Object> list(final Object... args) {
-            return new ArrayList<Object>(Arrays.asList(args));
-        }
-
 
         public List<Object> list(final List<Object> list, final Object... args) {
             list.addAll(Arrays.asList(args));
             return list;
         }
+
+        public List<Object> list(final Object... args) {
+            return new ArrayList<Object>(Arrays.asList(args));
+        }
+
     }
 }
