5c0e5d67018e16675150542a174f0e53d6ca915b
nickl- <github@jigsoft.co.za>
2019-03-19 15:54:46 +0200

    Improve parse error exception message.

--- src/main/java/bsh/ParseException.java
+++ src/main/java/bsh/ParseException.java
@@ -25,34 +25,6 @@
  * Author of Learning Java, O'Reilly & Associates                            *
  *                                                                           *
  *****************************************************************************/
-/*
-    This file was auto generated, but has been modified since then.  If we
-    need to regenerate it for some reason we should be careful to look at
-    the notes below.
-
-    All BeanShell modifications are demarcated by "Begin BeanShell
-    Modification - ... " and "End BeanShell Modification - ..."
-
-    Note: Please leave the ^M carriage return in the above auto-generated line
-    or JavaCC will complain about version on Win systems.
-
-    BeanShell Modification to generated file
-    ----------------------------------------
-
-    - Added sourceFile attribute
-        setErrorSourceFile()
-        getErrorSourceFile()
-    - Modified getMessage() to print more tersely except on debug
-      (removed "Was expecting one of...)
-    - Added sourceFile info to getMessage()
-    - Made ParseException extend EvalError
-    - Modified constructors to use EvalError
-    - Override
-        getErrorLineNumber()
-        getErrorText()
-    - Add
-        toString()
-*/
 package bsh;
 
 /**
@@ -64,10 +36,7 @@ package bsh;
  * You can modify this class to customize your error reporting
  * mechanisms so long as you retain the public fields.
  */
-public class ParseException
-// Begin BeanShell Modification - public, extend EvalError
-                            extends EvalError {
-// End BeanShell Modification - public, extend EvalError
+public class ParseException extends EvalError {
 
     /**
      * The version identifier for this Serializable class.
@@ -76,15 +45,14 @@ public class ParseException
      */
     private static final long serialVersionUID = 1L;
 
-//Begin BeanShell Modification - sourceFile
-    private static String sourceFile = "<unknown>";
+    private static String sourceFile = null;
 
     /**
      * Used to add source file info to exception
      * @param file source file
      */
     public void setErrorSourceFile(String file) {
-        sourceFile = file;
+        ParseException.sourceFile = file;
     }
 
     /**
@@ -93,14 +61,8 @@ public class ParseException
      */
     @Override
     public String getErrorSourceFile() {
-        return sourceFile;
+        return ParseException.sourceFile;
     }
-//End BeanShell Modification - sourceFile
-
-    /**
-     * The end of line string for this machine.
-     */
-    protected static String EOL = System.getProperty("line.separator", "\n");
 
     /**
      * This constructor is used by the method "generateParseException"
@@ -109,9 +71,7 @@ public class ParseException
      * "expectedTokenSequences", and "tokenImage" set.
      */
     public ParseException(Token currentTokenVal, int[][] expectedTokenSequencesVal, String[] tokenImageVal) {
-// Begin BeanShell Modification - constructor
         super(initialise(currentTokenVal, expectedTokenSequencesVal, tokenImageVal), null, null);
-// End BeanShell Modification - constructor
         currentToken = currentTokenVal;
         expectedTokenSequences = expectedTokenSequencesVal;
         tokenImage = tokenImageVal;
@@ -126,25 +86,19 @@ public class ParseException
      * relevant information. The JavaCC generated code does not use
      * these constructors.
      */
+
     public ParseException() {
-// Begin BeanShell Modification - constructor
-        super("", null, null);
-// End BeanShell Modification - constructor
+        this("");
     }
 
     /** Constructor with message. */
     public ParseException(String message) {
-// Begin BeanShell Modification - constructor
-        // null node, null callstack, ParseException knows where the error is.
         super(message, null, null);
-// End BeanShell Modification - constructor
     }
 
-// Begin BeanShell Modification - constructor
     public ParseException(String message, Throwable cause) {
         super(message, null, null, cause);
     }
-// End BeanShell Modification - constructor
 
     /**
      * This is the last token that has been consumed successfully. If
@@ -175,52 +129,30 @@ public class ParseException
      * gets displayed.
      */
     private static String initialise(Token currentToken, int[][] expectedTokenSequences, String[] tokenImage) {
+        StringBuilder retval = new StringBuilder("Unable to parse code syntax. Encountered:");
+        Token tok = currentToken;
+        while (null != (tok = tok.next))
+            retval.append(" ").append(add_escapes(tok.image));
 
-        StringBuilder expected = new StringBuilder();
-        int maxSize = 0;
-        for (int i = 0; i < expectedTokenSequences.length; i++) {
-            if (maxSize < expectedTokenSequences[i].length) {
-                maxSize = expectedTokenSequences[i].length;
-            }
-            for (int j = 0; j < expectedTokenSequences[i].length; j++) {
-                expected.append(tokenImage[expectedTokenSequences[i][j]]).append(' ');
-            }
-            if (expectedTokenSequences[i][expectedTokenSequences[i].length - 1] != 0) {
-                expected.append("...");
-            }
-            expected.append(EOL).append("    ");
-        }
-// Begin BeanShell Modification - added sourceFile info
-        String retval = "In file: "+ sourceFile +" Encountered: ";
-// End BeanShell Modification - added sourceFile info
-        Token tok = currentToken.next;
-        for (int i = 0; i < maxSize; i++) {
-            if (i != 0)
-                retval += " ";
-            if (tok.kind == 0) {
-                retval += tokenImage[0];
-                break;
-            }
-            retval += " " + tokenImage[tok.kind];
-            retval += " \"";
-            retval += add_escapes(tok.image);
-            retval += "\" ";
-            tok = tok.next;
-        }
-        retval += " at line " + currentToken.next.beginLine + ", column " + currentToken.next.beginColumn;
-        retval += "." + EOL;
-
-// Begin BeanShell Modification - made conditional on debug
+        retval.append(" at line ")
+            .append(currentToken.next.beginLine)
+            .append(", column ")
+            .append(currentToken.next.beginColumn);
+        if (null != sourceFile)
+            retval.append(" in: ").append(sourceFile);
+        
         if (Interpreter.DEBUG.get() && expectedTokenSequences.length != 0) {
-            if (expectedTokenSequences.length == 1)
-              retval += "Was expecting:" + EOL + "    ";
-            else
-              retval += "Was expecting one of:" + EOL + "    ";
-            retval += expected.toString();
+            retval.append(System.getProperty("line.separator", "\n")).append("Exppected");
+            if (expectedTokenSequences.length > 1)
+                retval.append(" one of");
+            retval.append(": ");
+            for (int i = 0; i < expectedTokenSequences.length; i++)
+                for (int j = 0; j < expectedTokenSequences[i].length; j++)
+                    retval.append(tokenImage[expectedTokenSequences[i][j]]
+                            .replace('"', ' ').trim()).append(' ');
         }
-// End BeanShell Modification - made conditional on debug
 
-        return retval;
+        return retval.toString();
     }
 
 
@@ -272,67 +204,24 @@ public class ParseException
     }
 
 
-// Begin BeanShell Modification - override error methods and toString
     @Override
     public int getErrorLineNumber() {
-        if (currentToken == null) {
-            String message = getMessage();
-            int index = message.indexOf(" at line ");
-            if (index > -1) {
-                message = message.substring(index + 9);
-                index = message.indexOf(',');
-                try {
-                    if (index == -1) {
-                        return Integer.parseInt(message);
-                    }
-                    return Integer.parseInt(message.substring(0, index));
-                } catch (NumberFormatException e) {
-                    // ignore, we have no valid line information, just return -1 for now
-                }
-            }
-            return -1;
-        }
         return currentToken.next.beginLine;
     }
 
+    @Override
     public String getErrorText() {
-        // copied from generated getMessage()
-        int maxSize = 0;
-        for (int i = 0; i < expectedTokenSequences.length; i++) {
-          if (maxSize < expectedTokenSequences[i].length)
-            maxSize = expectedTokenSequences[i].length;
-        }
-
-        String retval = "In file: "+ sourceFile +" Encountered ";
-        Token tok = currentToken.next;
-        for (int i = 0; i < maxSize; i++) {
-          if (i != 0) retval += " ";
-          if (tok.kind == 0) {
-            retval += tokenImage[0];
-            break;
-          }
-          retval += " " + tokenImage[tok.kind];
-          retval += " ";
-          retval += add_escapes(tok.image);
-          retval += " ";
-          tok = tok.next;
-        }
-        retval += " at line " + currentToken.next.beginLine + ", column " + currentToken.next.beginColumn;
-        retval += "." + EOL;
-
-        return retval;
+        return this.getMessage();
     }
 
     public String getMessage() {
-        return initialise(currentToken, expectedTokenSequences, tokenImage);
+        return ParseException.initialise(currentToken, expectedTokenSequences, tokenImage);
     }
     public String getMessage(boolean debug) {
         return this.getMessage();
     }
     public String toString() {
-        return getMessage();
+        return this.getMessage();
     }
-
-// End BeanShell Modification - override error methods and toString
 }
 /* JavaCC - OriginalChecksum=27381747f265700332a86284beda703e (do not edit this line) */
