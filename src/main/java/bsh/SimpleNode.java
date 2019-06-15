42c33defc0027f08bea44c8af9ec6000cc728317
nickl- <github@jigsoft.co.za>
2019-06-16 00:11:44 +0200

    Improved Node and SimpleNode.
    Improvements to Node interface and SimpleNode implementation to finally move away from cast to SimpleNode spagetti.
    Added many useful functions for accessing and manipulating the tree with check safety as well as a complete ListIterator implementation for traversal.

--- src/main/java/bsh/SimpleNode.java
+++ src/main/java/bsh/SimpleNode.java
@@ -23,12 +23,10 @@
  * Author of Learning Java, O'Reilly & Associates                            *
  *                                                                           *
  *****************************************************************************/
-
-
-
 package bsh;
 
 import java.io.Serializable;
+import java.util.NoSuchElementException;
 
 /*
     Note: great care (and lots of typing) were taken to insure that the
@@ -51,170 +49,179 @@ class SimpleNode implements Node, Serializable {
     /** Serialization ID */
     private static final long serialVersionUID = 1L;
 
-    public static SimpleNode JAVACODE =
-        new SimpleNode( -1 ) {
-            public String getSourceFile() {
-                return "<Called from Java Code>";
-            }
+    /** The first and last tokens */
+    Token firstToken, lastToken;
 
-            public int getLineNumber() {
-                return -1;
-            }
-
-            public String getText()  {
-                return "<Compiled Java Code>";
-            }
-        };
+    /** the source of the text from which this was parsed */
+    private String sourceFile;
 
     protected Node parent;
     protected Node[] children;
     protected int id;
-    protected Object value;
     protected Parser parser;
+    private int cursor = 0, lastRet = -1;
 
-    Token firstToken, lastToken;
+    /** Default constructor supplying the node with its type id.
+     * @param i type index of ParserTreeConstants.jjtNodeName */
+    public SimpleNode(int i) { id = i; }
 
-    /** the source of the text from which this was parsed */
-    String sourceFile;
+    /** {@inheritDoc} */
+    @Override
+    public boolean hasPrevious() { return cursor > 0; }
 
-    public SimpleNode(int i) {
-        id = i;
+    /** {@inheritDoc} */
+    @Override
+    public boolean hasNext() { return cursor < jjtGetNumChildren(); }
+
+    /** {@inheritDoc} */
+    @Override
+    public int nextIndex() { return cursor; }
+
+    /** {@inheritDoc} */
+    @Override
+    public int previousIndex() { return cursor - 1; }
+
+    /** {@inheritDoc} */
+    @Override
+    public Node next() {
+        if (!hasNext()) throw new NoSuchElementException();
+        return children[lastRet = cursor++];
     }
 
-    public SimpleNode(Parser p, int i) {
-      this(i);
-      parser = p;
+    /** {@inheritDoc} */
+    @Override
+    public Node previous() {
+        if (!hasPrevious()) throw new NoSuchElementException();
+        return children[lastRet = --cursor];
     }
 
+    /** {@inheritDoc} */
+    @Override
+    public void remove() {
+        if (lastRet < 0) throw new IllegalStateException();
+        cursor = lastRet;
+        Node c[] = new Node[children.length - 1];
+        System.arraycopy(children, 0, c, 0, cursor);
+        System.arraycopy(children, cursor + 1, c, cursor, c.length - cursor);
+        children = c;
+        lastRet = -1;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void set(Node e) {
+        if (lastRet < 0) throw new IllegalStateException();
+        children[lastRet] = e;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void add(Node e) {
+        Node c[] = new Node[jjtGetNumChildren() + 1];
+        System.arraycopy(children, 0, c, 0, cursor);
+        System.arraycopy(children, cursor, c, cursor +1, c.length - cursor -1);
+        children = c;
+        children[cursor++] = e;
+        lastRet = -1;
+    }
+
+    /** {@inheritDoc} */
+    @Override
     public void jjtOpen() { }
+
+    /** {@inheritDoc} */
+    @Override
     public void jjtClose() { }
 
+    /** {@inheritDoc} */
+    @Override
     public void jjtSetParent(Node n) { parent = n; }
-    public Node jjtGetParent() { return parent; }
-    //public SimpleNode getParent() { return (SimpleNode)parent; }
 
-    public void jjtAddChild(Node n, int i)
-    {
+    /** {@inheritDoc} */
+    @Override
+    public Node jjtGetParent() { return parent; }
+
+    /** {@inheritDoc} */
+    @Override
+    public void jjtAddChild(Node n, int i) {
         if (children == null)
             children = new Node[i + 1];
-        else
-            if (i >= children.length)
-            {
-                Node c[] = new Node[i + 1];
-                System.arraycopy(children, 0, c, 0, children.length);
-                children = c;
-            }
-
+        else if (i >= children.length) {
+            Node c[] = new Node[i + 1];
+            System.arraycopy(children, 0, c, 0, children.length);
+            children = c;
+        }
         children[i] = n;
     }
 
-    public Node jjtGetChild(int i) {
-        return children[i];
-    }
-    public SimpleNode getChild( int i ) {
-        return (SimpleNode)jjtGetChild(i);
-    }
+    /** {@inheritDoc} */
+    @Override
+    public Node jjtGetChild(int i) { return children[i]; }
 
+    /** {@inheritDoc} */
+    @Override
+    public Node[] jjtGetChildren() {
+        if ( null == children )
+            children = new Node[0];
+        return children;
+    }
+    /** {@inheritDoc} */
+    @Override
     public int jjtGetNumChildren() {
-        return (children == null) ? 0 : children.length;
+        return jjtGetChildren().length;
     }
 
-    public void jjtSetValue(Object value) { this.value = value; }
-    public Object jjtGetValue() { return value; }
-
-    /*
-        You can override these two methods in subclasses of SimpleNode to
-        customize the way the node appears when the tree is dumped.  If
-        your output uses more than one line you should override
-        toString(String), otherwise overriding toString() is probably all
-        you need to do.
-    */
+    /** {@inheritDoc} */
+    @Override
     public String toString() { return ParserTreeConstants.jjtNodeName[id]; }
+
+    /** {@inheritDoc} */
+    @Override
     public String toString(String prefix) { return prefix + toString(); }
 
-    /*
-        Override this method if you want to customize how the node dumps
-        out its children.
-    */
-    public void dump(String prefix)
-    {
+    /** {@inheritDoc} */
+    @Override
+    public void dump(String prefix) {
         System.out.println(toString(prefix));
-        if(children != null)
-        {
-            for(int i = 0; i < children.length; ++i)
-            {
-                SimpleNode n = (SimpleNode)children[i];
-                if (n != null)
-                {
-                    n.dump(prefix + " ");
-                }
-            }
+        if (children != null) for (int i = 0; i < children.length; ++i) {
+            SimpleNode n = (SimpleNode)children[i];
+            if (n != null)
+                n.dump(prefix + " ");
         }
     }
 
-    //  ---- BeanShell specific stuff hereafter ----  //
-
-    /**
-        Detach this node from its parent.
-        This is primarily useful in node serialization.
-        (see BSHMethodDeclaration)
-    */
-    public void prune() {
-        jjtSetParent( null );
-    }
-
-    /**
-        This is the general signature for evaluation of a node.
-    */
-    public Object eval( CallStack callstack, Interpreter interpreter )
-        throws EvalError
-    {
+    /** {@inheritDoc} */
+    @Override
+    public Object eval(CallStack callstack, Interpreter interpreter)
+            throws EvalError {
         throw new InterpreterError(
             "Unimplemented or inappropriate for " + getClass().getName() );
     }
 
-    /**
-        Set the name of the source file (or more generally source) of
-        the text from which this node was parsed.
-    */
-    public void setSourceFile( String sourceFile ) {
+    /** {@inheritDoc} */
+    @Override
+    public void setSourceFile(String sourceFile) {
         this.sourceFile = sourceFile;
     }
 
-    /**
-        Get the name of the source file (or more generally source) of
-        the text from which this node was parsed.
-        This will recursively search up the chain of parent nodes until
-        a source is found or return a string indicating that the source
-        is unknown.
-    */
+    /** {@inheritDoc} */
+    @Override
     public String getSourceFile() {
         if ( sourceFile == null )
             if ( parent != null )
-                return ((SimpleNode)parent).getSourceFile();
+                return parent.getSourceFile();
             else
                 return "<unknown file>";
         else
             return sourceFile;
     }
 
-    /**
-        Get the line number of the starting token
-    */
-    public int getLineNumber() {
-        return firstToken.beginLine;
-    }
+    /** {@inheritDoc} */
+    @Override
+    public int getLineNumber() { return firstToken.beginLine; }
 
-    /**
-        Get the ending line number of the starting token
-    public int getEndLineNumber() {
-        return lastToken.endLine;
-    }
-    */
-
-    /**
-        Get the text of the tokens comprising this node.
-    */
+    /** {@inheritDoc} */
+    @Override
     public String getText()
     {
         StringBuilder text = new StringBuilder();
@@ -232,9 +239,8 @@ class SimpleNode implements Node, Serializable {
         return text.toString();
     }
 
+    /** {@inheritDoc} */
     @Override
-    public int getId() {
-        return this.id;
-    }
+    public int getId() { return this.id; }
 }
 
