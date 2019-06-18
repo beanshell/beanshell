89d96d15634168a5bf2a1f83db2d8f0fc98be4b8
nickl- <github@jigsoft.co.za>
2019-06-18 02:51:42 +0200

    Improve BshScriptTestCase
    Improved the bsh test script test suite runner's verbosity output and other optimisations.
    Adding the maven argument `-Dverbose=true` will output each unit tests duration and outcome.

--- src/test/java/bsh/BshScriptTestCase.java
+++ src/test/java/bsh/BshScriptTestCase.java
@@ -21,7 +21,6 @@ package bsh;
 
 import static bsh.KnownIssue.KNOWN_FAILING_TESTS;
 import static bsh.KnownIssue.SKIP_KNOWN_ISSUES;
-import static java.lang.Boolean.valueOf;
 import static java.lang.System.err;
 import static java.lang.System.out;
 import static org.hamcrest.Matchers.containsString;
@@ -31,6 +30,7 @@ import static org.junit.Assume.assumeFalse;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FilenameFilter;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
@@ -48,12 +48,29 @@ import junit.framework.TestSuite;
 @RunWith(AllTestsJUnit4Runner.class)
 public class BshScriptTestCase {
 
+    /** System property verbose */
+    public static final boolean _VERBOSE =
+            Boolean.valueOf(System.getProperty("verbose"));
+    /** System property accessibility */
+    public static final boolean _ACCESSIBILITY =
+            Boolean.valueOf(System.getProperty("accessibility"));
+    /** System property script */
+    public static final String _SCRIPT = System.getProperty("script");
     /** The Constant baseDir. */
-    public static final File test_scripts_dir = new File(
+    public static final File _TEST_SCRIPTS_DIR = new File(
         "src/test/resources/test-scripts").getAbsoluteFile();
     static {
+        // List KNOWN_FAILING_TESTS here
         //KNOWN_FAILING_TESTS.add("Fail.bsh");
+
+        // Set capabilities accessibility
+        Capabilities.instance.accept(_ACCESSIBILITY);
     }
+    /** Constant padded spaces */
+    private static final String[] _PADS = new String[] {"", " ", "  ", "   "};
+    /** Constant tabs */
+    private static final String[] _TABS = new String[] {"", "\t", "\t\t",
+        "\t\t\t", "\t\t\t\t", "\t\t\t\t\t", "\t\t\t\t\t\t", "\t\t\t\t\t\t\t"};
 
     @Test
     public void testFailScript() throws Throwable {
@@ -63,15 +80,16 @@ public class BshScriptTestCase {
             assertThat("test expected to fail", e, isA(Error.class));
             assertThat("with message", e.getMessage(),
                     containsString("Test FAILED: Line: 11 : assert ( false )"));
-            if (valueOf(System.getProperty("verbose")))
-                System.out.println("Success");
+            if ( _VERBOSE )
+                System.out.println("success");
         } catch (Throwable t) {
             t.printStackTrace();
             throw t;
         }
     }
-    /** Suite.
-     * @return the junit.framework. test
+
+    /** Test suite entry method for framework.
+     * @return the unit test populated test suite.
      * @throws Exception the exception */
     public static junit.framework.Test suite() throws Throwable {
         final TestSuite suite = new TestSuite();
@@ -79,21 +97,24 @@ public class BshScriptTestCase {
         return suite;
     }
 
-    /** Adds the tests.
+    /** Adds the test scripts to the provided test suite.
      * @param suite the suite */
     private static void addTests(final TestSuite suite) {
-        final File[] files = test_scripts_dir.listFiles();
-        if (!isUnderScrutiny(suite) && files != null)
-            for (final File file : files) {
-                final String name = file.getName();
-                if (file.isFile() && name.endsWith(".bsh")
-                        && !"TestHarness.bsh".equals(name)
-                        && !"RunAllTests.bsh".equals(name)
-                        && !"Assert.bsh".equals(name)
-                        && !"Fail.bsh".equals(name)) {
+        if ( !isUnderScrutiny(suite) ) {
+            final File[] files = _TEST_SCRIPTS_DIR.listFiles(
+                new FilenameFilter() {
+                    public boolean accept(File dir, String name) {
+                        return name.endsWith(".bsh")
+                            && !"TestHarness.bsh".equals(name)
+                            && !"RunAllTests.bsh".equals(name)
+                            && !"Assert.bsh".equals(name)
+                            && !"Fail.bsh".equals(name);
+                    }
+                });
+            if ( null != files ) for ( final File file : files )
+                if ( file.isFile() )
                     suite.addTest(new Script(file));
-                }
-            }
+        }
     }
 
     /** Checks if is under scrutiny. Helper function to simplify singling out
@@ -102,7 +123,8 @@ public class BshScriptTestCase {
      * @param suite the suite
      * @return true, if is under scrutiny */
     private static boolean isUnderScrutiny(final TestSuite suite) {
-        final String trouble_maker = System.getProperty("script"); // test file(s) under scrutiny
+        // test file(s) under scrutiny
+        final String trouble_maker = _SCRIPT;
         if (trouble_maker.isEmpty())
             return false;
         for (String f:trouble_maker.split(","))
@@ -110,66 +132,76 @@ public class BshScriptTestCase {
         return true;
     }
 
-    /** The Class TestBshScript. */
+    /** The bsh script unit test case. */
     static class Script extends TestCase {
-
         /** The file. */
         private final File _file;
+        /** script timer **/
+        private long _start;
+        /** skipped test **/
+        private boolean _skipped = false;
 
         /** Instantiates a new test bsh script.
          * @param file the file */
         public Script(final File file) {
-            this._file = file;
+            super(file.getName());
+            _file = file;
         }
 
         /** Instantiates a new test bsh script.
          * @param script the script file name */
         public Script(String script) {
-            this(new File(test_scripts_dir, script));
+            this(new File(_TEST_SCRIPTS_DIR, script));
         }
 
-        /** {@inheritDoc} */
-        @Override
-        public String getName() {
-            return this._file.getName();
+        /** Get the duration stop time padded with spaces. */
+        private String stopTime() {
+            long stop = (System.nanoTime() - _start) / 1000000;
+            int cnt = 3 - (stop > 0 ? (int) Math.log10(stop) : 0);
+            return _PADS[cnt] +  stop + "ms";
         }
 
+        /** Determine if the exception is assumed for skipping a test.
+         * Either thrown locally from the list of known failed scripts or by
+         * script as the cause of the exception argument.
+         * @param assumption the exception argument to check.
+         * @throws Throwable re-thrown identified assumption. */
         private void skipAssumptions(Throwable assumption) throws Throwable {
-            while (null != assumption)
-                if ((assumption = assumption.getCause())
-                        instanceof AssumptionViolatedException) {
-                    if (valueOf(System.getProperty("verbose")))
-                        System.out.println("Skipped");
+            while ( null != assumption )
+                if ( assumption instanceof AssumptionViolatedException ) {
+                    if ( _VERBOSE ) {
+                        System.out.println(stopTime() + " - SKIPPED *");
+                        _skipped = true;
+                    }
                     throw assumption;
-                }
+                } else
+                    assumption = assumption.getCause();
         }
 
         /** {@inheritDoc} */
         @Override
         public void runTest() throws Throwable {
-            Capabilities.instance.accept(valueOf(System.getProperty("accessibility")));
-            assumeFalse("skipping test " + getName(), SKIP_KNOWN_ISSUES
-                    && KNOWN_FAILING_TESTS.contains(getName()));
-
-            long tStart = 0;
-            if (valueOf(System.getProperty("verbose"))) {
-                System.out.print(this.getName()+" - ");
-                tStart = System.nanoTime();
+            if ( _VERBOSE ) {
+                int len = 64 - getName().length();
+                int cnt = len / 8 - (len % 8 == 0 ? 1 : 0);
+                System.out.print(getName() + _TABS[cnt] + " - ");
+                _start = System.nanoTime();
             }
 
             try ( final Reader in = new InputStreamReader(
-                    new FileInputStream(this._file), StandardCharsets.UTF_8) ) {
+                    new FileInputStream(_file), StandardCharsets.UTF_8) ) {
                 final Interpreter interpreter = new Interpreter(in, out, err, false);
-                interpreter.sourceFileInfo = this.getName();
-                interpreter.set("bsh.cwd", test_scripts_dir.getPath());
-
                 try {
+                    assumeFalse("skipping test " + getName(), SKIP_KNOWN_ISSUES
+                            && KNOWN_FAILING_TESTS.contains(getName()));
+
+                    interpreter.sourceFileInfo = getName();
+                    interpreter.set("bsh.cwd", _TEST_SCRIPTS_DIR.getPath());
+
                     interpreter.eval(in);
-                    if (valueOf(System.getProperty("verbose")))
-                        System.out.print(((System.nanoTime()-tStart)/1000000)+"ms - ");
                 } catch (final Throwable e) {
                     skipAssumptions(e);
-                    if (!System.getProperty("script").isEmpty())
+                    if (!_SCRIPT.isEmpty())
                         e.printStackTrace(System.out);
                     throw new RuntimeException(getName(), e);
                 }
@@ -178,10 +210,20 @@ public class BshScriptTestCase {
                         (Boolean) interpreter.get("test_completed"));
                 assertFalse(""+interpreter.get("test_message"),
                         (Boolean) interpreter.get("test_failed"));
+
+            } catch (Throwable failed_exception) {
+                if ( _VERBOSE ) {
+                    if ( getName().equals("Fail.bsh") )
+                        System.out.print(stopTime() + " - ");
+                    else if ( !_skipped )
+                        System.out.println(stopTime() + " - FAILED **");
+                    _skipped = false;
+                }
+                throw failed_exception;
             }
 
-            if (valueOf(System.getProperty("verbose")))
-                System.out.println("Success");
+            if ( _VERBOSE )
+                System.out.println(stopTime() + " - success");
         }
     }
 }
