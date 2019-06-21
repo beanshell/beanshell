d0043371f9a618b2d14311c46f983c73a20a0b61
nickl- <github@jigsoft.co.za>
2019-06-21 13:08:07 +0200

    Optimised string manipulation.
    It is more efficient to do the new line replacements on the shortened source string instead of before.

--- src/main/java/bsh/Interpreter.java
+++ src/main/java/bsh/Interpreter.java
@@ -755,11 +755,9 @@ public class Interpreter
     }
 
     private String showEvalString( String s ) {
-        s = s.replace('\n', ' ');
-        s = s.replace('\r', ' ');
         if ( s.length() > 80 )
             s = s.substring( 0, 80 ) + " . . . ";
-        return s;
+        return s.replace('\n', ' ').replace('\r', ' ');
     }
 
     // end source and eval
