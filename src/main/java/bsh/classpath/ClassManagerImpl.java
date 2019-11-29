c3ce4d85bc9b707a085ba9d49f0cc52f0e8dbfe2
tmoore <tmoore@spatial.ca>
2019-11-29 11:36:02 -0500

    Handle the situation of a NoClassDefFoundError during class loading. Let the NoClassDefFoundError class loader error fall through so that BeanShell can continue with it's loading efforts.  If the class cannot be loaded than an EvalError will eventually be thrown.

--- src/main/java/bsh/classpath/ClassManagerImpl.java
+++ src/main/java/bsh/classpath/ClassManagerImpl.java
@@ -246,6 +246,45 @@ public class ClassManagerImpl extends BshClassManager
             } catch ( NoClassDefFoundError e ) {
                 // fall through
 */
+            } catch ( NoClassDefFoundError nce ) {
+               
+               /*
+                * This error can happen when the internal class name has a different
+                * spelling or capitalization than the external name of the resource
+                * that was loaded.  This can happen in a number or ways, but a common
+                * situation is case insensitive files systems like Windows, where a 
+                * request to load a class named 'a' may load a resource 'A.class' 
+                * where the internal class name of 'A' does not match 'a'.
+                *
+                * This can also happen on Windows OS when trying to reference a non-existing
+                * variable that has similar spelling but different capitalization than
+                * an imported class.  For example if a class named 'Foo' has been imported,
+                * and a script references 'foo', then this loader will try to load foo.class,
+                * which due to the case-insensitive file system will load Foo.class, and then
+                * the loader will throw the error because class in the file is Foo but it
+                * was looking for foo.
+                *
+                * What to do about this?  It is fundamentally a Java/Windows mismatch
+                * issue and BeanShell should not have to do anything.  However not 
+                * catching the Error causes the Interpreter to blow up which is unfriendly.
+                * I can think of three options of what to do:
+                *
+                * 1) Catch the error and silently fall through.  The next step after this
+                * will try to load the class by name from a Java source file, and maybe this 
+                * is what was intended.  If a matching java source file is not found then 
+                * ultimately an EvalError is thrown.
+                *
+                * 2) Same as above, but write a line to System.err indicating that a class
+                * resource had an mismatching internal name.  This might help to shed light
+                * when things are mysteriously not workig as expected.
+                * 
+                * 3) Wrap the exception in an EvalError.  This wont do any magic and will 
+                * put the burden on the developer to fix their problem if it is a Windows 
+                * case insensitivty problem.  However if there is a java source file that
+                * could be loaded then it wont happen, so this breaks things.  
+                */
+               
+               System.err.println("The class named '"+name+"' could not be found, either because it no longer exists or it is not contained in the class file of the same name. Caused by: "+nce.getMessage());
             }
 
         // Try scripted class
