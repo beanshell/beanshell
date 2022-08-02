7f19b8e63133e5d03e13d680d4079c08d29b6b00
nickl- <github@jigsoft.co.za>
2022-08-02 05:26:59 +0200

    Remove test console output.
    Prevent test from outputting debug info to console.

--- src/test/java/mypackage/MyClass.java
+++ src/test/java/mypackage/MyClass.java
@@ -2,10 +2,10 @@ package mypackage;
 
 public class MyClass {
     public MyClass() {
-        System.out.println("MyClass constructor: bar");
+        //System.out.println("MyClass constructor: bar");
     }
     public static void print( String s ) {
-        System.out.println(s);
+        //System.out.println(s);
     }
 }
 
