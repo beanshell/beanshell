1972d4c8336e96d36a6caedc98b1741e3bac281f
nickl- <github@jigsoft.co.za>
2022-12-04 19:15:16 +0200

    All number types for array arguments.
    Array dimensions accept all number types
    Array indexes accept all number types
    Array slices accept all number types
    List dimensions accept all number types
    List indexes accept all number types
    List slices accept all number types
    Improve exception messages and add cause.
    Improve assert for error message to include cause.
    Improve Primitive intValue to also cast for value.
    Added quick exit condition for cast to existing type.
    Added precision to Primitive boxType and unboxType.
    Optimize Types use of Primitive wrap and unwrap.
    Added extensive unit tests for all number types.
    
    Fixes #652 and #658

--- src/main/java/bsh/BSHArrayDimensions.java
+++ src/main/java/bsh/BSHArrayDimensions.java
@@ -117,13 +117,13 @@ class BSHArrayDimensions extends SimpleNode
             {
                 try {
                     Object length = jjtGetChild(i).eval(callstack, interpreter);
-                    definedDimensions[i] = ((Primitive)length).intValue();
+                    definedDimensions[i] = (int) Primitive.castWrapper(Integer.TYPE, length);
                 }
                 catch(Exception e)
                 {
                     throw new EvalError(
                         "Array index: " + i +
-                        " does not evaluate to an integer", this, callstack );
+                        " length does not evaluate to an integer", this, callstack, e );
                 }
             }
         }
