d5d3f146d8738def22cb776e5eff33a17e44749d
Jim Jagielski <jimjag@gmail.com>
2026-09-13 10:55:44 -0400

    Signal end of input from JavaCharStream without a stack trace
    The #551 fix now reaches every Parser, not only the one Interpreter wrapped.

--- src/main/java/bsh/Interpreter.java
+++ src/main/java/bsh/Interpreter.java
@@ -356,9 +356,9 @@ public class Interpreter
         this.console = console;
         if ( null == this.parser || get_jjtree().nodeArity() != 0
                 || (null != parent && parent.interactive) )
-            this.parser = new Parser(StacklessEofReader.wrap(getIn()));
+            this.parser = new Parser(getIn());
         else
-            this.parser.ReInit(StacklessEofReader.wrap(getIn()));
+            this.parser.ReInit(getIn());
     }
 
     /** Overloaded to accept a read only console.
@@ -553,7 +553,7 @@ public class Interpreter
                     e.printStackTrace();
                 if ( !interactive )
                     EOF = true;
-                parser.reInitInput(StacklessEofReader.wrap(getIn()));
+                parser.reInitInput(getIn());
             } catch (InterpreterError e) {
                 error("Internal Error: " + e.getMessage());
                 if ( !interactive )
@@ -581,7 +581,7 @@ public class Interpreter
                     fail.  Must re-init the char stream reader
                     (ASCII_UCodeESC_CharStream.java)
                 */
-                parser.reInitTokenInput(StacklessEofReader.wrap(getIn()));
+                parser.reInitTokenInput(getIn());
 
                 if( !interactive )
                     EOF = true;
